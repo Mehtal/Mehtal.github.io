b063c4b8f45490736ce9ed7ef3483a7d6e101dda
khalil <mehtalkhalil@gmail.com>
2017-12-21 17:02:06 +0100

    e

---
+++ js/custom.js
@@ -0,0 +1,212 @@
+/* Show and hide menu */
+
+$(document).ready(function(){
+   
+    'use strict';
+    
+    $(window).scroll(function() {
+        
+        'use strict';
+        
+        if($(window).scrollTop() < 80 ) {
+            
+            $('.navbar-default').css ({
+               'padding-top': '20px',
+               'padding-bottom': '20px',
+                
+            });
+            
+                        
+        } else {
+            
+            $('.navbar').css ({
+               
+                
+            });
+            
+            $('.navbar-default').css({
+                'padding-top': '0px',
+                'padding-bottom': '0px',
+                'border-color': '#000'
+                
+            });
+            
+            $('.navbar-brand img').css({
+               'height': '35px',
+                'padding-top': '0px'
+                
+            });
+            
+            $('.navbar-nav > li > a ').css({
+                'padding-top': '15px'
+                
+                
+            });
+            
+            
+        }
+       
+        
+    });
+    
+    
+});
+
+//add smooth scrolling
+$(document).ready(function() {
+    
+    'use strict';
+   
+    
+    $('.nav-item, #scroll-to-top').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+    
+    
+});
+
+/* active menu item on click */
+$(document).ready(function() {
+    
+    'use strict';
+    
+    
+    $('.navbar-nav li a').click(function(){
+        
+        'use strict';
+        
+        $('.navbar-nav li a').parent().removeClass("active");
+        
+        $(this).parent().addClass("active");
+    
+    });
+});
+
+// highlight menu item on scroll
+$(document).ready(function() {
+    
+    'use strict';
+    
+    $(window).scroll( function() {
+        
+        'use strict';
+        
+        $("section").each(function()  {
+            
+            'use strict';
+            
+            var bb = $(this).attr("id");  // ABOUT, CONTACT, DOWNLOAD
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+            
+            if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+             
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+           
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+                
+            }
+            
+            
+        });
+    
+    
+    });
+    
+    
+});
+
+// add auto padding to header 
+
+$(document).ready(function() {
+    
+    'use strict';
+    
+    setInterval( function() {
+        
+        'use strict';
+        
+        var windowHeight = $(window).height();
+        
+        var containerHeight = $(".header-container").height();
+        
+        var padTop = windowHeight - containerHeight;
+        
+        $(".header-container").css({
+            
+            'padding-top': Math.round( padTop / 2) + 'px',
+            'padding-bottom': Math.round( padTop / 2) + 'px'
+            
+        });
+        
+        
+    }, 10)
+    
+    
+});
+
+
+
+// Add animation/ Initialize Woo
+$(document).ready(function() {
+    
+    'use strict';
+   
+    new WOW().init();
+    
+});
+
+
+// multiple frame carousel
+
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+    interval: 10000
+    })
+    
+    $('#myCarousel').on('slid.bs.carousel', function() {
+        //alert("slid");
+    });
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/media.js
@@ -0,0 +1,4 @@
+if (screen.width >= 600) {
+	// download complicated script
+	// swap in full-source images for low-source ones
+}
